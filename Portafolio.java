a89d7c597fdeb22afbca81b518fe3b45feafea64
jrubi531 <jersoto@scl.edu.gt>
2025-03-18 09:30:52 -0600

    Add files via upload

---
+++ Portafolio.java
@@ -0,0 +1,45 @@
+public class Portafolio {
+
+    public static void main(String[] args) {
+        
+        
+
+                System.out.println("     _ ____        _     __");
+                System.out.println("    | |  _ \\ _   _| |__ /_/");
+                System.out.println(" _  | | |_) | | | | '_ \\| |");
+                System.out.println("| |_| |  _ <| |_| | |_) | |");
+                System.out.println(" \\___/|_| \\_\\\\__,_|_.__/|_|");
+                System.out.println(                                      );
+            
+
+           System.out.println("selecciona una opcion");
+           System.out.println("proyecto iniciado ---> Marzo 18 a las 8:45 am 2025");
+
+           System.out.println( " |-----------Menú-----------|");
+           System.out.println(" | [ 1 ] Suma de un numero ");
+           System.out.println( " | [ 2 ]  ");
+           System.out.println( " | [ 3 ]  ");
+           System.out.println( " | [ 4 ] salida ");
+          
+
+
+
+   
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
